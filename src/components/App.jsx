4c5aea92850a8cb2993956e9a3cfe47fcf7dd81d
Damian <dcamp090799@gmail.com>
2025-07-22 21:37:53 -0400

    Refactor project structure by moving components to a dedicated folder and reintroducing styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
+import Header from "./Header";
+import Main from "./Main";
 
 function App() {
 
   return (
     <>
-      <h1>Hello Vite!</h1>
-      <p>This is my first Vite project!</p>
+      <Header />
+      <Main />
     </>
   )
 }
---
+++ src/components/Main.jsx
@@ -0,0 +1,7 @@
+function Main() {
+  return (
+    <p>This is my first Vite project!</p>
+  );
+};
+
+export default Main;
