1a1ab24fee6697f3cad2530dd780d2e72a7afec2
Damian <dcamp090799@gmail.com>
2025-07-22 22:57:49 -0400

    Add Animal component and constants for animal images
    - Created Animal component to display images based on selected animal.
    - Added constants file with data for various animals including a default image.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Header from "./Header";
 import Main from "./Main";
+import Animal from './Animal';
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <div className="page">
       <Header />
       <Main />
+      <Animal />
     </div>
   )
 }
---
+++ src/utils/constants.js
@@ -0,0 +1,14 @@
+export const data = [
+  { 
+    name: "cat",
+    image: new URL("../assets/animals/tt-cat.jpg", import.meta.url).href
+  },
+  { 
+    name: "hummingbird",
+    image: new URL("../assets/animals/tt-hummingbird.jpg", import.meta.url).href
+  },
+  { 
+    name: "default",
+    image: new URL("../assets/animals/tt-default.jpg", import.meta.url).href
+  }
+];
